7b2b0dae5be4f1d4abb3a2eef120e2036a12d8f8
harshalyallewar <harshalyallewar2@gmail.com>
2023-06-17 23:23:19 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,68 @@
+Front-end:
+The front-end, also known as the client-side, refers to the part of a website that is visible to user. 
+It involves the technologies like HTML, CSS, JavaScript and components that users interact with directly. 
+Front-End Technologies : 
+- HTML : HTML stands for Hypertext Markup Language. In this some tags and the elements are used to create the structure of the web page.
+- CSS : CSS stands for Cascading Style Sheets. CSS is used to style the HTML webpages.
+- JavaScript: JavaScript is a scripting language that runs in the web browser.
+  It is use to provides functionality in web pages.
+
+Back-end:
+The back-end, also known as the server-side. 
+It involves the server, database, and the server-side programming that manage and process data. 
+Back-End Technologies :
+
+- Node.js : Node.js is a JavaScript runtime environment built on Chrome's V8 JavaScript engine. 
+ It allows developers to run JavaScript on the server-side. It enable developer to create web applications. 
+- Express.js : Express.js is a framework for Node.js. It is use to handle HTTP requests and responses. 
+- MongoDB : MongoDB is a NoSQL database that stores data in JSON-like documents. It is highly scalabile.
+
+HTML tags are building blocks use to create the structure of webpages. They are enclosed in angle brackets "<>".
+
+- Structural Tags: 
+<html>: This tag represents the root element of an HTML document.
+<head>: It contains meta-information about the webpage, such as the title, links for CSS stylesheets.
+<body>: This contains tags which are shown on the webpage.
+- Heading Tags:
+These tags are used to define headings.
+<h1> to <h6>. <h1> being the highest level and <h6> the lowest.
+- Paragraph Tags:
+These tags are used to define paragraphs in webpage. <p>
+- Link Tags:
+These tags are used to create hyperlinks to other webpages. <a>
+- Image Tags:
+These tags are used to insert images into a webpage. <img>
+
+The Virtual DOM (VDOM) is a technique used in JavaScript frameworks like React to optimize the 
+rendering the user interface of a web application.
+- Initial Rendering:
+When a React component is first rendered, it creates a virtual representation of the entire DOM hierarchy,
+known as the Virtual DOM.
+- Virtual DOM Representation:
+The Virtual DOM consists of elements or components with their associated properties and attributes, similar to the actual DOM. 
+ However, the Virtual DOM is purely a JavaScript object and does not have any real impact on the web page.
+- Update to the Actual DOM:
+ Rather than updating each individual DOM element, React applies the changes in a batched manner, 
+ minimizing the number of direct manipulations to the DOM.
+ 
+- MySQL : MySQL is the Relational Database System. 
+ It stores the data in the form of tables and 
+ each tabke contains Fiels and Rows. It is vertically scalable.
+ It Handle Structured data.
+ Examples : MySql, Oracle.
+
+- NoSQL : NoSQL is the Non Relational Database System. 
+ It store the data in form of key-value pair.
+ it is horizontally scalable.
+ It Handle unstructured data.
+ Examples : MongoDB, RavenDB.
+
+ MongoDB
+ It is a NoSQL database System. MongoDB stores data in JSON-like format 
+ or we can say in key-value pair known as BSON (Binary JSON). 
+ which allows for dynamic schemas and easy representation of complex data structures.
+ MongoDB provides functionality for managing and organizing large amounts of structured and unstructured data.
+ MQL stands for MongoDB Query Language which is use to manipulating data stored in MongoDB 
+ in a flexible and efficient manner.
+ MongoDB database is highly Scalable and follows horizontal approach.
+ Generally, MongoDB is use for the development of MERN Stack Web Application.
--- form/src/App.js
+++ form/src/App.js
@@ -1,162 +1,33 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    number: "",
-    year: "",
-    department: "",
-    dob: "",
-    age: "",
-    caste: "",
-    state: "",
-    city: "",
-    gender: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    // Add your logic to handle form submission here
-  };
-
   return (
-    <div className="container">
-      <h2>Student Admission Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
+    <div className="Body">
+      <div className="App">
+        <h1>Admission Form</h1>
+        <div className="form-container">
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
 
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
+            <label htmlFor="age">Age:</label>
+            <input type="number" id="age" name="age" required />
 
-        <div className="form-group">
-          <label htmlFor="number">Number:</label>
-          <input
-            type="text"
-            name="number"
-            id="number"
-            value={formData.number}
-            onChange={handleChange}
-          />
-        </div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
 
-        <div className="form-group">
-          <label htmlFor="year">Year:</label>
-          <input
-            type="text"
-            name="year"
-            id="year"
-            value={formData.year}
-            onChange={handleChange}
-          />
-        </div>
+            <label htmlFor="course">Course:</label>
+            <select id="course" name="course">
+              <option value="btech">B.Tech</option>
+              <option value="mba">MBA</option>
+              <option value="msc">M.Sc</option>
+            </select>
 
-        <div className="form-group">
-          <label htmlFor="department">Department:</label>
-          <input
-            type="text"
-            name="department"
-            id="department"
-            value={formData.department}
-            onChange={handleChange}
-          />
+            <button type="submit">Submit</button>
+          </form>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="dob">Date of Birth:</label>
-          <input
-            type="text"
-            name="dob"
-            id="dob"
-            value={formData.dob}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="age">Age:</label>
-          <input
-            type="text"
-            name="age"
-            id="age"
-            value={formData.age}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="caste">Caste:</label>
-          <input
-            type="text"
-            name="caste"
-            id="caste"
-            value={formData.caste}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="state">State:</label>
-          <input
-            type="text"
-            name="state"
-            id="state"
-            value={formData.state}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            value={formData.city}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="gender">Gender:</label>
-          <select
-            name="gender"
-            id="gender"
-            value={formData.gender}
-            onChange={handleChange}
-          >
-            <option value="">Select</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
   );
 }
